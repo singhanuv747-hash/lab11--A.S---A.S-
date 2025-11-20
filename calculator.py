cdff2ad31d49fb01968a555f8b056787f145e70e
singhanuv747-hash <singhanuv747@gmail.com>
2025-11-19 23:58:10 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,37 @@
 # https://github.com/singhanuv747-hash/lab11--A.S---A.S-.git
-# Partner 1: <Anuv Singh>
+# Partner 1: <Your Name>
 # Partner 2: None
 
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Square root not defined for negative numbers")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return a / b
+def divide(a, b):
+    if a == 0:  
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0:
-        raise ValueError("Logarithm undefined for a <= 0.")
+        raise ValueError("Logarithm undefined for this input")
     if b <= 0 or b == 1:
-        raise ValueError("Invalid logarithm base.")
+        raise ValueError("Invalid base")
     return math.log(a, b)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
