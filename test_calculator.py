ac33f8a4243c10067a7402f53e0b5f8dc6c1ece3
singhanuv747-hash <singhanuv747@gmail.com>
2025-11-19 23:58:37 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,54 +7,31 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
 
-    # Partner 2
-    def test_add(self):
-        self.assertEqual(add(3, 4), 7)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
-
-    def test_subtract(self):
-        self.assertEqual(sub(10, 3), 7)
-        self.assertEqual(sub(0, 5), -5)
-        self.assertEqual(sub(-5, -5), 0)
-
-    # Partner 1
+    # Partner 1 tests you must complete
     def test_multiply(self):
-        self.assertEqual(mul(3, 4), 12)
-        self.assertEqual(mul(0, 5), 0)
-        self.assertEqual(mul(-2, 3), -6)
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(0, 5), 0)
+        self.assertEqual(multiply(-2, 3), -6)
 
     def test_divide(self):
-        self.assertEqual(div(10, 2), 5)
-        self.assertEqual(div(-9, 3), -3)
-        self.assertEqual(div(5, 1), 5)
+        self.assertEqual(divide(2, 10), 5)  
+        self.assertEqual(divide(5, 25), 5)
+        self.assertEqual(divide(-1, 5), -5)
 
-    # Partner 2
-    def test_divide_by_zero(self):
-        with self.assertRaises(ZeroDivisionError):
-            div(5, 0)
-
-    def test_logarithm(self):
-        self.assertEqual(log(8, 2), 3)
-        self.assertAlmostEqual(log(100, 10), 2)
-        self.assertAlmostEqual(log(27, 3), 3)
-
-    def test_log_invalid_base(self):
-        with self.assertRaises(ValueError):
-            log(10, 1)
-
-    # Partner 1
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            log(-5, 2)
+            logarithm(-3, 2)
 
     def test_hypotenuse(self):
-        # You do NOT have a hypotenuse function, so comment these out
-        pass
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(1, 1), math.sqrt(2))
 
     def test_sqrt(self):
-        # You do NOT have a sqrt function, so comment these out
-        pass
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        self.assertEqual(square_root(0), 0)
+        self.assertEqual(square_root(9), 3)
 
 # Do not touch this
 if __name__ == "__main__":
