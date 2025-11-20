83e72e595b5f64a5558990b2168d3d4483220213
singhanuv747-hash <singhanuv747@gmail.com>
2025-11-19 23:55:48 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,29 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/singhanuv747-hash/lab11--A.S---A.S-.git
+# Partner 1: <Anuv Singh>
+# Partner 2: None
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Logarithm undefined for a <= 0.")
+    if b <= 0 or b == 1:
+        raise ValueError("Invalid logarithm base.")
+    return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
